0da7d981137aaadf0b00ec11d6acf6ce8ca7044b
Samuel Lewis <samuel.lewis1@gmail.com>
2020-09-16 19:22:36 -0400

    Add files via upload

---
+++ HW 2.py
@@ -0,0 +1,108 @@
+# Samuel Lewis
+# HW 2
+# Machine Learning Class
+# I dowloaded the anaconda pack to run this
+
+# Read in all of the data
+import pandas as pd
+import math
+import numpy
+import matplotlib.pyplot as plt
+import seaborn as sns
+#%matplotlib inline
+
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.impute import KNNImputer
+from sklearn import tree
+import pydotplus
+import matplotlib.image as pltimg
+from sklearn.model_selection import train_test_split, cross_val_score
+
+
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+#%matplotlib inline
+train_df = pd.read_csv('train.csv')
+test_df = pd.read_csv('test.csv')
+combine = [train_df, test_df]
+
+corr_matrix = train_df.corr()
+print(corr_matrix)
+
+
+# Question 1
+print(train_df.describe())
+# Drop the unnecessary features
+train_df = train_df.drop("Name", axis = 1)
+train_df = train_df.drop("Ticket", axis = 1)
+train_df = train_df.drop("Cabin", axis = 1)
+train_df = train_df.drop("PassengerId", axis = 1)
+
+# Change the Sex to a numerical system
+train_df['Sex'] = train_df['Sex'].map({'female':1,'male': 0}).astype(int)
+train_df['Embarked'].fillna(train_df['Embarked'].dropna().mode()[0], inplace = True)
+train_df['Embarked'] = train_df['Embarked'].map({'Q':1,'S': 0, 'C':2}).astype(int)
+
+# Fill in the Age to avoid losing data
+train_df['Age'].fillna(train_df['Age'].dropna().median(), inplace = True)
+
+# Fill in the Fare to avoid losing data
+train_df['Fare'].fillna(train_df['Fare'].dropna().mean(), inplace = True)
+train_df = train_df.astype(int)
+test = train_df.Survived
+other = train_df.drop("Survived", axis = 1)
+features = other.columns
+
+
+# Question 2
+# Feature extraction - Chi Square
+# Code caused some errors when run but returns the results of the feature selection algorithm
+# Comment the code out to have the rest run
+array = train_df.values
+X = array[:,0:7]
+Y = array[:,7]
+test = SelectKBest(score_func=chi2, k=4)
+fit = test.fit(X, Y)
+numpy.set_printoptions(precision=3)
+print(fit.scores_)
+features = fit.transform(X)
+print(train_df.columns.values)
+print(features[0:5,:])
+
+
+# Question 4
+dtree = DecisionTreeClassifier(criterion = "gini", max_depth = 3)
+dtree = dtree.fit(other, test)
+cvs = cross_val_score(dtree,other,test,cv=5)
+print("Decision Tree five-fold : ")
+print(cvs.mean())
+
+# Question 3
+data = tree.export_graphviz(dtree, out_file=None, feature_names=features)
+graph = pydotplus.graph_from_dot_data(data)
+graph.write_png('mydecisiontree.png')
+
+img=pltimg.imread('mydecisiontree.png')
+imgplot = plt.imshow(img)
+plt.show()
+
+# Question 5
+
+dtree = RandomForestClassifier(random_state=30, max_depth = 3)
+dtree = dtree.fit(other, test)
+cvs = cross_val_score(dtree,other,test,cv=5)
+print("Random Forest 5 fold : ")
+print(cvs.mean())
+
+# Random stuff for Question 1 mainly
+corr_matrix = train_df.corr()
+print(corr_matrix)
+print(train_df.describe())
